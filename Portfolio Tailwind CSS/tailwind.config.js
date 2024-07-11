7522156f159ef46bced8c63a069be19915dcbf82
vaishali <vaishu@gmail.com>
2024-07-11 18:05:08 +0530

    my first commit

---
+++ Portfolio Tailwind CSS/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html", "./public/*.js"], 
+   theme: {
+   
+    extend: {
+      colors:{
+        'pink':'#FC6D6D',
+        'react':'#61DAFB',
+        'bootstrap':'#804ab8',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
+
